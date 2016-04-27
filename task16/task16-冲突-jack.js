a81dc05cffdb1146d9478a971b7eed84fae71941
zhangjie <327585907@qq.com>
2016-04-27 08:39:25 +0800

    任务描述

--- task16/task16-冲突-jack.js
+++ task16/task16-冲突-jack.js
@@ -6,16 +6,12 @@
  *    "上海": 40
  * };
  */
-
+//简写getElementById（）方法为 $()
 function $(id) {
     return document.getElementById(id);
 }
 
 var aqiData = {};
-var cityInp = $("aqi-city-input");
-var dataInp = $("aqi-value-input");
-var addBtn = $("add-btn");
-var aqiTable = $("aqi-table");
 
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
@@ -23,14 +19,12 @@ var aqiTable = $("aqi-table");
  */
 
 function addAqiData() {
-    var city = cityInp.value.trim();
-    var data = dataInp.value.trim();
-    var reCity = /[^A-Za-z\u4e00-\u9fa5]+/g;
-    var reData = /\D|\s/g;
-    if (city && city.match(reCity)) {
+    var city = $("aqi-city-input").value.trim();
+    var data = $("aqi-value-input").value.trim();
+    if (!city.match(/^[A-Za-z\u4e00-\u9fa5]+$/)) {
         alert("城市名必须为中英文字符！");
         return false;
-    } else if (!data || data.match(reData)) {
+    } else if (!data.match(/^\d+$/)) {
         alert("空气质量指数必须为整数！");
         return false;
     } else {
@@ -43,10 +37,10 @@ function addAqiData() {
  */
 function renderAqiList() {
     var temp = "<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>";
-     for(var p in aqiData){
-        temp+="<tr><td>" + p +"</td><td>" + aqiData[p] + "</td><td><button>删除</button></td></tr>";
+    for (var city in aqiData) {
+        temp += "<tr><td>" + city + "</td><td>" + aqiData[city] + "</td><td><button>删除</button></td></tr>";
     }
-    aqiTable.innerHTML = temp;
+    $("aqi-table").innerHTML = temp;
 }
 
 /**
@@ -62,18 +56,29 @@ function addBtnHandle() {
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
 //  */
-function delBtnHandle() {
+function delBtnHandle(target) {
     // do sth.
-    delete aqiData;
+    var currentRow = target.parentNode.parentNode;
+    var city = currentRow.cells[0].innerHTML;
+    delete aqiData[city];
     renderAqiList();
 }
+
 
 function init() {
 
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-    addBtn.onclick = addBtnHandle;
-    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+    //$("add-btn").onclick = addBtnHandle;
+    $("add-btn").addEventListener("click", addBtnHandle);
 
+    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+    var delBtn = $("aqi-table").getElementsByTagName("button");
+    $("aqi-table").addEventListener("click", function(event) {
+        if (event.target && event.target.nodeName.toLowerCase() === "button") {
+            delBtnHandle(event.target);
+        }
+    });
 }
+
 
 init();
--- task17/task17.js
+++ task17/task17.js
@@ -11,44 +11,48 @@ var aqiSourceData = {
 
 // 以下两个函数用于随机模拟生成测试数据
 function getDateStr(dat) {
-  var y = dat.getFullYear();
-  var m = dat.getMonth() + 1;
-  m = m < 10 ? '0' + m : m;
-  var d = dat.getDate();
-  d = d < 10 ? '0' + d : d;
-  return y + '-' + m + '-' + d;
+    var y = dat.getFullYear();
+    var m = dat.getMonth() + 1;
+    m = m < 10 ? '0' + m : m;
+    var d = dat.getDate();
+    d = d < 10 ? '0' + d : d;
+    return y + '-' + m + '-' + d;
 }
+
 function randomBuildData(seed) {
-  var returnData = {};
-  var dat = new Date("2016-01-01");
-  var datStr = ''
-  for (var i = 1; i < 92; i++) {
-    datStr = getDateStr(dat);
-    returnData[datStr] = Math.ceil(Math.random() * seed);
-    dat.setDate(dat.getDate() + 1);
-  }
-  return returnData;
+    var returnData = {};
+    var dat = new Date("2016-01-01");
+    var datStr = ''
+    for (var i = 1; i < 92; i++) {
+        datStr = getDateStr(dat);
+        returnData[datStr] = Math.ceil(Math.random() * seed);
+        dat.setDate(dat.getDate() + 1);
+    }
+    return returnData;
 }
 
 var aqiSourceData = {
-  "北京": randomBuildData(500),
-  "上海": randomBuildData(300),
-  "广州": randomBuildData(200),
-  "深圳": randomBuildData(100),
-  "成都": randomBuildData(300),
-  "西安": randomBuildData(500),
-  "福州": randomBuildData(100),
-  "厦门": randomBuildData(100),
-  "沈阳": randomBuildData(500)
+    "北京": randomBuildData(500),
+    "上海": randomBuildData(300),
+    "广州": randomBuildData(200),
+    "深圳": randomBuildData(100),
+    "成都": randomBuildData(300),
+    "西安": randomBuildData(500),
+    "福州": randomBuildData(100),
+    "厦门": randomBuildData(100),
+    "沈阳": randomBuildData(500)
 };
+/*for(var p in aqiSourceData["北京"]){
+  console.log(p +":"+aqiSourceData["北京"][p]);
+}*/
 
 // 用于渲染图表的数据
 var chartData = {};
 
 // 记录当前页面的表单选项
 var pageState = {
-  nowSelectCity: -1,
-  nowGraTime: "day"
+    nowSelectCity: -1,
+    nowGraTime: "day"
 }
 
 /**
@@ -62,22 +66,44 @@ function renderChart() {
  * 日、周、月的radio事件点击时的处理函数
  */
 function graTimeChange() {
-  // 确定是否选项发生了变化 
+    // 确定是否选项发生了变化 
+    var formTime = document.getElementById("form-gra-time");
+    var graTime = formTime.elements["gra-time"];
+    var sTime = null;
+    for (var i = 0; i < graTime.length; i++) {
+        graTime[i].onclick = function() {
+            if (this.checked) {
+                sTime = this.value;
+            } else {
+                this.checked = true;
+            }
+        }
+    }
+    if(sTime= "day"){
+      return aqiSourceData;
+    }else if(sTime=="week"){
+     
+      for(var p in aqiSourceData[city]){
+        for(i=0;i<7;i++){
+          week
+        }
 
-  // 设置对应数据
+      }
+    }
+    // 设置对应数据
 
-  // 调用图表渲染函数
+    // 调用图表渲染函数
 }
 
 /**
  * select发生变化时的处理函数
  */
 function citySelectChange() {
-  // 确定是否选项发生了变化 
+    // 确定是否选项发生了变化 
 
-  // 设置对应数据
+    // 设置对应数据
 
-  // 调用图表渲染函数
+    // 调用图表渲染函数
 }
 
 /**
@@ -91,9 +117,9 @@ function initGraTimeForm() {
  * 初始化城市Select下拉选择框中的选项
  */
 function initCitySelector() {
-  // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
+    // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
 
-  // 给select设置事件，当选项发生变化时调用函数citySelectChange
+    // 给select设置事件，当选项发生变化时调用函数citySelectChange
 
 }
 
@@ -101,17 +127,17 @@ function initCitySelector() {
  * 初始化图表需要的数据格式
  */
 function initAqiChartData() {
-  // 将原始的源数据处理成图表需要的数据格式
-  // 处理好的数据存到 chartData 中
+    // 将原始的源数据处理成图表需要的数据格式
+    // 处理好的数据存到 chartData 中
 }
 
 /**
  * 初始化函数
  */
 function init() {
-  initGraTimeForm()
-  initCitySelector();
-  initAqiChartData();
+    initGraTimeForm()
+    initCitySelector();
+    initAqiChartData();
 }
 
 init();
